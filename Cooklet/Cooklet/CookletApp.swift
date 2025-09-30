df282d4dc42c872abfe440cc2bb12a17ee40a158
Charu Hasini Tiruveedhula <tiruveedhulacharuhasini@gmail.com>
2025-09-30 14:05:34 +0530

    Add files via upload

---
+++ Cooklet/Cooklet/CookletApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookletApp.swift
+//  Cooklet
+//
+//  Created by Nxtwave on 30/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CookletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
